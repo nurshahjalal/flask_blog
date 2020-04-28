65a952340c6f4f6be14b0dbb0a99a05e65149c02
Nur.Shahjalal <Nur.Shahjalal@sandata.com>
2020-04-28 02:24:08 -0400

    updating profile pic code

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,3 +1,6 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, logout_user, current_user, login_required
 from flaskblog import app, db, bcrypt
@@ -74,13 +77,56 @@ def logout():
     return redirect(url_for('home'))
 
 
+def save_image(form_picture):
+    path_to_save = os.path.join(app.root_path, "static/profile_pics/")
+    print(path_to_save)
+    # save image with random number
+    random_hex = secrets.token_hex(8)
+    # getting the extension of user's requested pic file
+    _, pic_ext = os.path.splitext(form_picture.filename)
+    pic_filename = random_hex + pic_ext
+    # setting the save path
+    # saving_destination = os.path.join(app.root_path, "static/profile_pics/" + pic_filename)
+    saving_destination = path_to_save + pic_filename
+    print(saving_destination)
+
+    # resize the pics
+    output_size = (125, 125)
+    img = Image.open(form_picture)
+    img.thumbnail(output_size)
+    img.save(saving_destination)
+
+    return pic_filename
+
+
 @app.route("/account", methods=['GET', 'POST'])
 # @login_required is for to go to account user needs to log in
 @login_required
 def account():
     form = UpdateAccountForm()
+    # if user wants to update username and email or any of them
     if form.validate_on_submit():
+        # save profile pics if user wants to change
+        if form.picture_file.data:
+            # change the  profile image
+            profile_image = save_image(form.picture_file.data)
+            # Save the  profile image to DB under current user
+            current_user.image_file = profile_image
+
+        # replacing the current username with requested username
         current_user.username = form.username.data
+        # replacing the current email with requested email
         current_user.email = form.email.data
+        # commit to DB
+        db.session.commit()
+        # flash message to HTML page
+        flash("Your Account has been updated", "success")
+        return redirect(url_for('account'))
+
+    # this is for prepopulated form
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
     image = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image, form=form)
